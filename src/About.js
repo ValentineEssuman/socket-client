e6fafaea6010c1cc78533ab621354549c2ad383a
Justice V. Essuman <jessuman@ppalgh.com>
2020-07-14 17:02:56 +0000

    added files forreact frontend for socket data visualization

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import App from './App';
+import './App.css';
+
+export const About = () => (
+    <Container className ="uptop">
+        <Jumbotron>
+            <h2 classsName="header">God Bless Ghana</h2>
+            <p>Ghana Covid 19 Status Info to The Zongo</p>
+        </Jumbotron>
+    </Container>
+);
---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component} from 'react';
+import  {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import socketIOClient from "socket.io-client";
+//import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import  { About }  from './About';
+import  { Layout } from './Componet/Layout'
+import { NavigationBar } from './NavigationBar'
+
+//import logo from './logo.svg';
+//import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      response: false,
+      endpoint: "http://localhost:4001"
+    };
+  }
+
+  componentDidMount(){
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("API", data => this.setState({response: data}));
+  }
+
+  render(){
+    const { response } = this.state;
+    return (
+      <React.Fragment>
+        <NavigationBar/>
+        <About/>
+        
+            {response ? <p>{ response }</p>: <p>Loading....</p>}
+      
+        
+      </React.Fragment>
+      
+    )
+  }
+}
+
+
+export default App;
