9f8e3f2ec4b6f30e3cbd9b4332adb388cd311c21
Justice V. Essuman <jessuman@ppalgh.com>
2020-08-11 11:16:38 +0000

    Changed name ins src to test jsmodbus integration for a SCADA appliacation

--- src/About.js
+++ src/About.js
@@ -7,8 +7,8 @@ import './App.css';
 export const About = () => (
     <Container className ="uptop">
         <Jumbotron>
-            <h2 classsName="header">God Bless Ghana</h2>
-            <p>Ghana Covid 19 Status Info to The Zongo</p>
+            <h2 classsName="header">JS to Modbus</h2>
+            {/* <p>Ghana Covid 19 Status Info to The Zongo</p> */}
         </Jumbotron>
     </Container>
 );
